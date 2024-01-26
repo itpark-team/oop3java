56a2146712b51b538d85eccf80f66bcf16b0a02f
amsmyslov <a.m.smyslov@yandex.ru>
2024-01-26 18:30:58 +0300

    Initial commit

---
+++ src/SimpleMath.java
@@ -0,0 +1,9 @@
+public class SimpleMath {
+    public int getSum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int getMul(int num1, int num2) {
+        return num1 * num2;
+    }
+}
---
+++ src/TrigMath.java
@@ -0,0 +1,8 @@
+public class TrigMath {
+    private double pi = 3.14159265359;
+
+    public double getCircleSquare(double radius) {
+        //return pi * radius * radius;
+        return pi * Math.pow(radius, 2);
+    }
+}
